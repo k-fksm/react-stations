b798e6014a283b578013f48845ece5398bc95482
福島皓平 <kfksm@prrrrrrrrrro.local>
2021-06-04 13:42:40 +0900

    edit station 4

--- src/App.js
+++ src/App.js
@@ -9,13 +9,17 @@ import './App.css'
  */
 export const App = () => {
   const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/collie-border/n02106166_3447.jpg');
+  const
   return (
     <div>
       <header>
         Dogアプリ
       </header>
       犬の画像を表示するサイトです。
-      <button onClick={() => setDogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg')}>
+      <button onClick={fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => setDogUrl(data.message))
+      }>
         更新
       </button>
       <img src={dogUrl}>
