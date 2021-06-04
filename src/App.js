142d7a61e41ad10d1253333f38064cb95b857c96
福島皓平 <kfksm@prrrrrrrrrro.local>
2021-06-04 13:57:57 +0900

    edit station 6

--- src/App.js
+++ src/App.js
@@ -8,20 +8,25 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
-  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/collie-border/n02106166_3447.jpg');
+  const [dogUrl, setDogUrl] = React.useState('https://images.dog.ceo/breeds/collie-border/n02106166_3447.jpg');
   return (
     <div>
       <header>
         Dogアプリ
       </header>
-      犬の画像を表示するサイトです。
-      <button onClick={fetch('https://dog.ceo/api/breeds/image/random')
-        .then(response => response.json())
-        .then(data => setDogUrl(data.message))
-      }>
-        更新
-      </button>
-      <img src={dogUrl}>
+      <main>
+        犬の画像を表示するサイトです。
+        <button onClick={
+          fetch('https://dog.ceo/api/breeds/image/random')
+            .then(response => response.json())
+            .then(data => {
+              if(data.status === "success"){
+                setDogUrl(data.message)}})
+        }>
+          更新
+        </button>
+        <img src={dogUrl}>
+      </main>
     </div>
   )
 }
