6d7c56646d76cf29c1955fb4142d6aca5943d7a4
福島皓平 <kfksm@prrrrrrrrrro.local>
2021-06-04 12:54:33 +0900

    edit station 4

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ export const App = () => {
   const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/collie-border/n02106166_3447.jpg');
   return (
     <div>
-      <header>
-        Dogアプリ
-      </header>
-      犬の画像を表示するサイトです。
+      //<header>
+      //  Dogアプリ
+      //</header>
+      //犬の画像を表示するサイトです。
       <img src={dogUrl}>
     </div>
   )
