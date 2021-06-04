8784b096cfbfc3aaee26dcb5d0824d61e934d86b
福島皓平 <kfksm@prrrrrrrrrro.local>
2021-06-04 12:00:11 +0900

    edit station 4

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
-  const [dogUrl, setDogUrl] = setState('https://images.dog.ceo/breeds/collie-border/n02106166_3447.jpg');
+  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/collie-border/n02106166_3447.jpg');
   return (
     <div>
       <header>
